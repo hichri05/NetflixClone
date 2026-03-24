4dfd99e32a187c02a0676555b357669bfaeb7c1f
noura <khazrinoura67@gmail.com>
2026-03-24 20:46:03 +0100

    je suis entrein de coder les services

--- src/main/java/org/netflix/Models/Serie.java
+++ src/main/java/org/netflix/Models/Serie.java
@@ -1,16 +1,8 @@
 package org.netflix.Models;
 
-import java.util.List;
-
-public class Serie extends Media{
-    private int nbrSaison;
-    public Serie(int idMedia, String title, String description, int releaseYear,
-                 double averageRating, String coverImageUrl, String director,
-                 int nbrSaison, List<Genre> genres) {
-        super( idMedia, title, description,releaseYear,
-                averageRating,  coverImageUrl, director, genres);
-        this.nbrSaison = nbrSaison;
-    }
-    public int getNbrSaison() { return nbrSaison; }
-    public void setNbrSaison(int nbrSaison) { this.nbrSaison = nbrSaison; }
+public class Serie{
+    private int id;
+    private String title;
+    private String description;
+    private String posterImage;
 }
