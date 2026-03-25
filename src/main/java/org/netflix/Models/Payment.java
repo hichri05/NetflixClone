4685a1c2ef783c82ba65399d5a1ba480b8f86f6d
nour <nourelhoudalatrech9@gmail.com>
2026-03-25 21:42:53 +0100

    MovieDaO complete

---
+++ src/main/java/org/netflix/Models/Payment.java
@@ -0,0 +1,10 @@
+package org.netflix.Models;
+
+public class Payment {
+    private int id_payment;
+    private int id_User;
+    private int id_Subscription;
+    private float amount;
+    private String currency;
+    private int Status;
+}
