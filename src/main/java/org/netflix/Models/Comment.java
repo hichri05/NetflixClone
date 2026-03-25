3ec1c1128e203a055b46b8f21d7d9bb284c9333b
nour <nourelhoudalatrech9@gmail.com>
2026-03-25 22:48:56 +0100

    rating+ comments (entities

---
+++ src/main/java/org/netflix/Models/Comment.java
@@ -0,0 +1,39 @@
+package org.netflix.Models;
+
+import java.time.LocalDate;
+
+public class Comment {
+    public int id_Comment;
+    public int id_User;
+    public int id_Media;
+    public String content;
+    public LocalDate created_at;
+    public int is_reported;
+    public Comment(int id_Comment, int id_User, int id_Media, String content, LocalDate created_at, int is_reported) {
+        this.id_Comment = id_Comment;
+        this.id_User = id_User;
+        this.id_Media = id_Media;
+        this.content = content;
+        this.created_at = created_at;
+        this.is_reported = is_reported;
+    }
+    public Comment(int id_User, int id_Media, String content) {
+        this.id_User = id_User;
+        this.id_Media = id_Media;
+        this.content = content;
+        this.created_at = LocalDate.now();
+        this.is_reported = 0; // Par défaut, un commentaire n'est pas signalé
+    }
+    public int getId_Comment() { return id_Comment; }
+    public int getId_User() { return id_User; }
+    public int getId_Media() { return id_Media; }
+    public String getContent() { return content; }
+    public LocalDate getCreated_at() { return created_at; }
+    public int getIs_reported() { return is_reported; }
+    public void setId_Comment(int id_Comment) { this.id_Comment = id_Comment; }
+    public void setId_User(int id_User) { this.id_User = id_User; }
+    public void setId_Media(int id_Media) { this.id_Media = id_Media; }
+    public void setContent(String content) { this.content = content; }
+    public void setCreated_at(LocalDate created_at) { this.created_at = created_at; }
+    public void setIs_reported(int is_reported) { this.is_reported = is_reported; }
+}
---
+++ src/main/java/org/netflix/Models/Rating.java
@@ -0,0 +1,35 @@
+package org.netflix.Models;
+
+import java.time.LocalDate;
+
+public class Rating {
+    public int id_Rating;
+    public int id_User;
+    public int id_Media;
+    public float score;
+    public LocalDate ratingDate;
+    public Rating(int id_Rating, int id_User, int id_Media, float score, LocalDate ratingDate) {
+        this.id_Rating = id_Rating;
+        this.id_User = id_User;
+        this.id_Media = id_Media;
+        this.score = score;
+        this.ratingDate = ratingDate;
+    }
+    public Rating(int id_User, int id_Media, float score, LocalDate ratingDate) {
+        this.id_User = id_User;
+        this.id_Media = id_Media;
+        this.score = score;
+        this.ratingDate = ratingDate;
+    }
+    public int getId_Rating() { return id_Rating; }
+    public int getId_User() { return id_User; }
+    public int getId_Media() { return id_Media; }
+    public float getScore() { return score; }
+    public LocalDate getRatingDate() { return ratingDate; }
+    public void setId_Rating(int id_Rating) { this.id_Rating = id_Rating; }
+    public void setId_User(int id_User) { this.id_User = id_User; }
+    public void setId_Media(int id_Media) { this.id_Media = id_Media; }
+    public void setScore(float score) { this.score = score; }
+    public void setRatingDate(LocalDate ratingDate) { this.ratingDate = ratingDate; }
+
+}
