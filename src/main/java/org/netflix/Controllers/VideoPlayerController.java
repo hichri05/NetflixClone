ab7caf3c8ddf742b4b3fac19801dfb044e8129e4
Saski703 <hichriomar036@gmail.com>
2026-03-25 04:59:48 +0100

    add-VideoPLayer

---
+++ src/main/java/org/netflix/Controllers/VideoPlayerController.java
@@ -0,0 +1,76 @@
+package org.netflix.Controllers;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+import javafx.util.Duration;
+import org.netflix.Utils.SceneSwitcher;
+
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class VideoPlayerController implements Initializable {
+    @FXML private MediaView mediaView;
+    @FXML private Button playBtn;
+    @FXML private Slider timeSlider;
+    @FXML private Slider volumeSlider;
+    @FXML private Label videoTitle;
+    private MediaPlayer mediaPlayer;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        String vd = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
+        Media media = new Media(vd);
+        mediaPlayer = new MediaPlayer(media);
+        mediaView.setMediaPlayer(mediaPlayer);
+        Platform.runLater(() -> {
+            if (mediaView.getScene() != null) {
+                mediaView.fitWidthProperty().bind(mediaView.getScene().widthProperty());
+                mediaView.fitHeightProperty().bind(mediaView.getScene().heightProperty());
+            }
+        });
+        mediaPlayer.currentTimeProperty().addListener((obs, oldTime, newTime) -> {
+            if (!timeSlider.isValueChanging()) {
+                timeSlider.setValue(newTime.toSeconds());
+            }
+        });
+        mediaPlayer.setOnReady(() -> {
+            timeSlider.setMax(mediaPlayer.getTotalDuration().toSeconds());
+        });
+        mediaPlayer.volumeProperty().bind(volumeSlider.valueProperty().divide(100));
+        mediaPlayer.play();
+    }
+
+    @FXML
+    public void handleBack(ActionEvent event) {
+        mediaPlayer.stop();
+        SceneSwitcher.goTo(event, "/org/netflix/Views/Home.fxml");
+    }
+
+    public void handleRewind(ActionEvent actionEvent) {
+        mediaPlayer.seek(mediaPlayer.getCurrentTime().subtract(Duration.seconds(10)));
+    }
+
+    public void handleForward(ActionEvent actionEvent) {
+        mediaPlayer.seek(mediaPlayer.getCurrentTime().add(Duration.seconds(10)));
+    }
+
+    public void togglePlay(ActionEvent actionEvent) {
+        if (mediaPlayer.getStatus() == MediaPlayer.Status.PLAYING) {
+            mediaPlayer.pause();
+            playBtn.setText("▶");
+        } else {
+            mediaPlayer.play();
+            playBtn.setText("⏸");
+        }
+    }
+}
