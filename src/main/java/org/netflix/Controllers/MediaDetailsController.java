4521b29844e1eb00015ef91ff6b1889b27eaf469
Saski703 <hichriomar036@gmail.com>
2026-04-10 23:07:59 +0100

    ...

--- src/main/java/org/netflix/Controllers/MediaDetailsController.java
+++ src/main/java/org/netflix/Controllers/MediaDetailsController.java
@@ -13,6 +13,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
