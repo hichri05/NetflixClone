2cd4843af287167c4b0d481dad2fdb9c41c8e3b8
nour <nourelhoudalatrech9@gmail.com>
2026-04-17 23:40:42 +0100

    na7i f comments

--- src/main/java/org/netflix/Main/App.java
+++ src/main/java/org/netflix/Main/App.java
@@ -33,13 +33,13 @@ public class App extends Application {
 
     public static void setRoot(String fxml) throws IOException {
         scene.setRoot(loadFXML(fxml));
-    }
+    }//
 
     private static Parent loadFXML(String fxml) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/org/Views/"+fxml + ".fxml"));
         return fxmlLoader.load();
     }
-//
+
     public static void main(String[] args) {
         for (Movie m: MovieDAO.getAllMovies()) {
             System.out.println(m);
